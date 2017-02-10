de4fbab01e5f45c03ffcd030261de332aa3a1c0e
jdougal84 <jdougal84@aim.com>
2017-02-09 22:57:05 -0500

    MatLib Plot Added

--- Analysis.py
+++ Analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def test_run():
     df = pd.read_csv("Data/HCP.csv")
@@ -14,8 +15,8 @@ def test_run():
 def get_max_close(symbol):
     '''Return the maxium closign value for a stock indicated by symbol'''
 
-    df = pd.read_csv("Data/{}.csv".format(symbol)) #Reads in data and accepts an arg to determine which file
-    return df['Close'].max()    #Compute and return max from close column
+    df = pd.read_csv("Data/{}.csv".format(symbol))  #Reads in data and accepts an arg to determine which file
+    return df['Close'].max()                        #Compute and return max from close column
 
 def test_run_close():
 
@@ -23,8 +24,16 @@ def test_run_close():
         print("Max Close")
         print(symbol,get_max_close(symbol))
 
+def test_run_matlib():
+
+    df = pd.read_csv("Data/APPL.csv")
+    print(df['Adj Close'])
+    df['Adj Close'].plot()
+    plt.show()                  #Must be called to show plots
+
 if __name__ == "__main__":
     #test_run()
-    test_run_close()
+    #test_run_close()
+    test_run_matlib()
 
-    #compute 0:27
+    #compute 0:27ls
